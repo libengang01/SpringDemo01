3c6123c34dccbb8ef7037c9307d93399e79a46c7
tom_glb <tom_glb@qq.com>
2020-05-31 22:55:25 +0800

    增加账单类型

---
+++ src/main/java/com/example/demo/service/impl/BillServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.IBillService;
+
+public class BillServiceImpl implements IBillService {
+    @Override
+    public int insert() {
+        return 0;
+    }
+}
